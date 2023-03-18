0f4a6e911c844299ced61cee00d39e332d5fa746
propaan <legrandenzo.pro@gmail.com>
2023-03-18 21:11:50 +0100

    add swap bloc

---
+++ precache-manifest.4399caf7b53322c9ac34c0b5b5ae6f05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5730fbfe1ad5767da2f1",
+    "url": "./static/css/main.a0198ca8.chunk.css"
+  },
+  {
+    "revision": "5730fbfe1ad5767da2f1",
+    "url": "./static/js/main.5730fbfe.chunk.js"
+  },
+  {
+    "revision": "0e7fb873d39e287def2f",
+    "url": "./static/js/1.0e7fb873.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "d69ae68331c5f7b44bc778dafbf42fc2",
+    "url": "./static/media/streetline.d69ae683.otf"
+  },
+  {
+    "revision": "c9d8d9a0f9e393e5b30ca9364f1e4940",
+    "url": "./index.html"
+  }
+];
